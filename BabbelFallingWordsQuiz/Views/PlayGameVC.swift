6576d329afd4fadc1f9570326d11a536782750d2
Ireshika Piyumalie <ipiyumalie@gmail.com>
2019-06-09 14:30:33 +0200

    ÏP_PlayGameVC

---
+++ BabbelFallingWordsQuiz/Views/PlayGameVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlayGameVC.swift
+//  BabbelFallingWordsQuiz
+//
+//  Created by Bhashitha on 09.06.19.
+//  Copyright © 2019 Ireshika Piyumalie. All rights reserved.
+//
+
+import UIKit
+
+class PlayGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

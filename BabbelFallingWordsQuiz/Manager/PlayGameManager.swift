57e4ef1ca28f067715c6073a1bd8e21f512660e9
Ireshika Piyumalie <ipiyumalie@gmail.com>
2019-06-09 15:11:29 +0200

    ÏP_Added Manager class for func implementation

---
+++ BabbelFallingWordsQuiz/Manager/PlayGameManager.swift
@@ -0,0 +1,9 @@
+//
+//  PlayGameManager.swift
+//  BabbelFallingWordsQuiz
+//
+//  Created by Ireshika on 09.06.19.
+//  Copyright © 2019 Ireshika Piyumalie. All rights reserved.
+//
+
+import Foundation

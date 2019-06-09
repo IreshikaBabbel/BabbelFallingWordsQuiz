931a95711d8ac65166253068753feb4a5bcaa8d9
Ireshika Piyumalie <ipiyumalie@gmail.com>
2019-06-09 17:03:32 +0200

    ÏP_Modified launch screen image

---
+++ BabbelFallingWordsQuiz/Views/CustomViews/FallingCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  FallingCustomView.swift
+//  BabbelFallingWordsQuiz
+//
+//  Created by Bhashitha on 09.06.19.
+//  Copyright © 2019 Ireshika Piyumalie. All rights reserved.
+//
+
+import UIKit
+
+class FallingCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
